3df89278f330b491e8a052305ea4871deaf3e6e0
Jay <jaredreyes039@gmail.com>
2024-04-17 13:48:15 -0700

    More fixes

---
+++ build/scene1.d
@@ -0,0 +1,1 @@
+scene1.o: /home/jaydev/Development/3DSDeviceHealthMonitor/source/scene1.c
---
+++ includes/scene0.h
@@ -0,0 +1,11 @@
+static void initScene0(void);
+static void renderHeader0(void);
+static void renderTimeInterface(void);
+static void renderTouchInterface(void);
+static void renderJoystickInterface(void);
+static void renderGyroInterface(void);
+static void renderSliderInterface(void);
+static void renderFirmwareInterface(void);
+static void compileInterfaces(void);
+static void buildScene0(float size);
+static void exitScene0(void);
